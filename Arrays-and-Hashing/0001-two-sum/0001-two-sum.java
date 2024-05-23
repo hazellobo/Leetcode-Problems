273d7c33e85772e57c7963a942f111c56a340916
Hazel Lobo <lobo.ha@northeastern.edu>
2024-05-23 13:12:31 -0400

    0001-two-sum

---
+++ Arrays-and-Hashing/0001-two-sum/0001-two-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int n = nums.length;
+
+        for( int i =0; i< n; i++){
+            int num = target - nums[i];
+            if(map.containsKey(num)){
+                return new int[]{map.get(num),i};
+            }
+            map.put(nums[i],i);
+        }
+            return new int[]{};
+
+    }
+}
